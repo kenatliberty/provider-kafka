d67fd45df0a21230e523577c88252bb55487006a
Ken Lavoie <ken.lavoie@libertymutual.com>
2022-02-22 13:59:22 -0500

    Update topic_test.go

--- internal/controller/topic/topic_test.go
+++ internal/controller/topic/topic_test.go
@@ -9,7 +9,10 @@ import (
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/twmb/franz-go/pkg/kadm"
+	"k8s.io/client-go/util/workqueue"
 	"reflect"
+	controllerruntime "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"testing"
 )
 
@@ -18,7 +21,7 @@ var dataTesting = []byte(`{
 	"sasl": {
 	"mechanism": "PLAIN",
 	"username": "user",
-	"password": ""
+	"password": "P0Jn0ZK9MA"
 	}
 	}`)
 
@@ -28,16 +31,11 @@ func TestObserve(t *testing.T) {
 
 	type fields struct {
 		kafkaClient *kadm.Client
-		log         logging.Logger
 	}
 	type args struct {
 		ctx context.Context
 		mg  resource.Managed
 	}
-
-	//l := logging.Logger()
-
-	logger := logging.Logger.Info("info-ng")
 
 	cases := map[string]struct {
 		name    string
@@ -49,9 +47,8 @@ func TestObserve(t *testing.T) {
 		"ObserveT1": {
 			fields: fields{
 				kafkaClient: newAc,
-				log:         logger,
 			},
-			name: "Working1",
+			name: "testTopic-1",
 			args: args{
 				ctx: context.Background(),
 				// need to look at styra and compare how they did it
@@ -71,10 +68,7 @@ func TestObserve(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &external{
 				kafkaClient: tt.fields.kafkaClient,
-				// the type
-				log: tt.fields.log,
 			}
-			fmt.Print(c)
 			got, err := c.Observe(tt.args.ctx, tt.args.mg)
 			fmt.Print(got)
 			if (err != nil) != tt.wantErr {
@@ -83,6 +77,206 @@ func TestObserve(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Observe() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSetup(t *testing.T) {
+	type args struct {
+		mgr controllerruntime.Manager
+		l   logging.Logger
+		rl  workqueue.RateLimiter
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Setup(tt.args.mgr, tt.args.l, tt.args.rl); (err != nil) != tt.wantErr {
+				t.Errorf("Setup() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_connector_Connect(t *testing.T) {
+	type fields struct {
+		kube         client.Client
+		usage        resource.Tracker
+		log          logging.Logger
+		newServiceFn func(creds []byte) (*kadm.Client, error)
+	}
+	type args struct {
+		ctx context.Context
+		mg  resource.Managed
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    managed.ExternalClient
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &connector{
+				kube:         tt.fields.kube,
+				usage:        tt.fields.usage,
+				log:          tt.fields.log,
+				newServiceFn: tt.fields.newServiceFn,
+			}
+			got, err := c.Connect(tt.args.ctx, tt.args.mg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Connect() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Connect() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_external_Create(t *testing.T) {
+	type fields struct {
+		kafkaClient *kadm.Client
+		log         logging.Logger
+	}
+	type args struct {
+		ctx context.Context
+		mg  resource.Managed
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    managed.ExternalCreation
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &external{
+				kafkaClient: tt.fields.kafkaClient,
+				log:         tt.fields.log,
+			}
+			got, err := c.Create(tt.args.ctx, tt.args.mg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Create() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_external_Delete(t *testing.T) {
+	type fields struct {
+		kafkaClient *kadm.Client
+		log         logging.Logger
+	}
+	type args struct {
+		ctx context.Context
+		mg  resource.Managed
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &external{
+				kafkaClient: tt.fields.kafkaClient,
+				log:         tt.fields.log,
+			}
+			if err := c.Delete(tt.args.ctx, tt.args.mg); (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_external_Observe(t *testing.T) {
+	type fields struct {
+		kafkaClient *kadm.Client
+		log         logging.Logger
+	}
+	type args struct {
+		ctx context.Context
+		mg  resource.Managed
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    managed.ExternalObservation
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &external{
+				kafkaClient: tt.fields.kafkaClient,
+				log:         tt.fields.log,
+			}
+			got, err := c.Observe(tt.args.ctx, tt.args.mg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Observe() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Observe() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_external_Update(t *testing.T) {
+	type fields struct {
+		kafkaClient *kadm.Client
+		log         logging.Logger
+	}
+	type args struct {
+		ctx context.Context
+		mg  resource.Managed
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    managed.ExternalUpdate
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &external{
+				kafkaClient: tt.fields.kafkaClient,
+				log:         tt.fields.log,
+			}
+			got, err := c.Update(tt.args.ctx, tt.args.mg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Update() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Update() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
